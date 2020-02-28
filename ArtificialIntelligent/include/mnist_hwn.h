3aa7e729caa9d00a8d5cad51d12400e710a9e87b
Seoncheol Kim <skim@cinemo.com>
2020-02-28 14:55:19 +0100

    added file open code #2

--- ArtificialIntelligent/include/mnist_hwn.h
+++ ArtificialIntelligent/include/mnist_hwn.h
@@ -1,19 +1,29 @@
 #ifndef _MNIST_HWN_H_
 #define _MNIST_HWN_H_
 
-typedef struct _IMAGESET_FILE_
+typedef struct _IMAGESET_FILE_INFO_
 {
     int magicNumber;
     int numberOfImage;
     int numberOfRows;
     int numberOfColumns;
+}ImageSetFileInfo;
+
+typedef struct _IMAGESET_FILE_
+{
+    ImageSetFileInfo info;
     unsigned char **data;
 }ImageSetFile, *pImageSetFile;
 
-typedef struct _LABELSET_FILE_
+typedef struct _LABELSET_FILE_INFO_
 {
     int magicNumber;
     int numberOfItems;
+}LabelSetFileInfo;
+
+typedef struct _LABELSET_FILE_
+{
+    LabelSetFileInfo info;
     unsigned char *label;
 }LabelSetFile, *pLabelSetFile;
 
--- ArtificialIntelligent/main.c
+++ ArtificialIntelligent/main.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "include/linkedlist.h"
+//#include "include/linkedlist.h"
 #include "include/mnist_hwn.h"
 
-void readImageSetFile(char *path, pImageSetFile info);
-void readLabelSetFile(char *path, pLabelSetFile info);
+#define errorReturn(FILE_POINTER) \
+do{ \
+printf("*error occured in %s:%d\n", __FILE__ , __LINE__); \
+fclose(FILE_POINTER); \
+return; \
+} while(0);
+
+void readImageSetFile(char *path, pImageSetFile _info);
+void readLabelSetFile(char *path, pLabelSetFile _info);
 
 int main(char argc, char *argv[])
 {
@@ -21,44 +28,56 @@ int main(char argc, char *argv[])
     return 0;
 }
 
-void readImageSetFile(char *path, pImageSetFile info)
+void readImageSetFile(char *path, pImageSetFile _info)
 {
     FILE *pf = NULL;
     int i;
+    int sum = 0;
 
     pf = fopen(path, "rb");
 
-    fread(info, sizeof(ImageSetFile) - 4, 1, pf);
-    if(info->magicNumber != 0x00000803) // 2051 magic number MSB
+    if(fread(&_info->info, sizeof(ImageSetFileInfo), 1, pf) < 0)
+        errorReturn(pf);
+    _info->info.magicNumber = be32toh(_info->info.magicNumber);
+    _info->info.numberOfImage = be32toh(_info->info.numberOfImage);
+    _info->info.numberOfRows = be32toh(_info->info.numberOfRows);
+    _info->info.numberOfColumns = be32toh(_info->info.numberOfColumns);
+
+    if(_info->info.magicNumber != 0x00000803) // 2051 magic number MSB
     {
         return;
     }
 
-    info->data = (unsigned char**)malloc(info->numberOfImage);
-    for(i = 0; i < info->numberOfImage; i++)
+    _info->data = (unsigned char**)malloc(_info->info.numberOfImage);
+    for(i = 0; i < _info->info.numberOfImage; i++)
     {
-        info->data[i] = (unsigned char*)malloc(info->numberOfRows * info->numberOfColumns);
-        fread(info->data[i],sizeof(unsigned char), info->numberOfRows * info->numberOfColumns, pf);
+        _info->data[i] = (unsigned char*)malloc(_info->info.numberOfRows * _info->info.numberOfColumns);
+        if(fread(_info->data[i],sizeof(unsigned char), _info->info.numberOfRows * _info->info.numberOfColumns, pf) < 0)
+            errorReturn(pf);
+        sum += _info->info.numberOfRows * _info->info.numberOfColumns;
     }
 
     fclose(pf);
+    pf = NULL;
 }
 
 
-void readLabelSetFile(char *path, pLabelSetFile info)
+void readLabelSetFile(char *path, pLabelSetFile _info)
 {
     FILE * pf = NULL;
 
     pf = fopen(path, "rb");
 
-    fread(info, sizeof(LabelSetFile) - 4, 1, pf);
-    if(info->magicNumber != 0x00000801) // 2049 magic number MSB
+    fread(&_info->info, sizeof(LabelSetFile), 1, pf);
+    _info->info.magicNumber = be32toh(_info->info.magicNumber);
+    _info->info.numberOfItems = be32toh(_info->info.numberOfItems);
+    if(_info->info.magicNumber != 0x00000801) // 2049 magic number MSB
     {
         return;
     }
-    info->label = (unsigned char*)malloc(info->numberOfItems);
+    _info->label = (unsigned char*)malloc(_info->info.numberOfItems);
 
-    fread(info->label, sizeof(unsigned char), info->numberOfItems, pf);
+    fread(_info->label, sizeof(unsigned char), _info->info.numberOfItems, pf);
 
     fclose(pf);
 }
