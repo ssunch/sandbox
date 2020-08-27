1dec4f7ab04b5c384515aba4efe46479531d3c72
Hyung Seok <utpjb@student.kit.edu>
2020-08-27 16:42:58 +0200

    i used two buffers for each but i am not sure that the first one works well

--- xwindow/imageViewer/viewer.c
+++ xwindow/imageViewer/viewer.c
@@ -48,17 +48,21 @@ static const char *event_names[] = {
 };
 
 void refresh(Display* d, Window window);
+void visual(Display* display, Window window, Image Buffer);
 unsigned long GetPixelValue(unsigned char red, unsigned char green, unsigned char blue);
 void imageScaler(pImage dest, pImage src, double ratio);
 ErrorState fitToWindowScale(pImage dest, pImage src);
+ErrorState fixedfitToWindowScale(pImage dest, pImage src);
 void BMPbufferToRGB(pImage dest, char *filename);
 void JPGbufferToRGB(pImage dest, char *filename);
 void initMemory(void *p, size_t size);
 void copyToWindowBuffer(pImage src);
 
 static Image windowImageBuffer;
+static Image windowImageBuffer_2;
 static Image originImage;
 static Image scaledImg;
+static Image fixedscaledImg;
 
 int main(int argc, char** argv) {
 
@@ -111,8 +115,20 @@ int main(int argc, char** argv) {
     windowImageBuffer.width = width;
     windowImageBuffer.height = height;
 
+    memcpy(&windowImageBuffer_2, &windowImageBuffer, sizeof(Image));
+
+    printf("%d, %d", windowImageBuffer_2.height, windowImageBuffer_2.width);
     // Create window
     Window window = XCreateSimpleWindow(display, parent_window,
+                                              x,
+                                              y,
+                                              width,
+                                              height,
+                                              border_width,
+                                              border_color,
+                                              background_color);
+
+    Window window_1 = XCreateSimpleWindow(display, parent_window,
                                               x,
                                               y,
                                               width,
@@ -136,26 +152,33 @@ int main(int argc, char** argv) {
 
     // Select window events
     XSelectInput(display, window, event_mask);
+    XSelectInput(display, window_1, event_mask);
 
     // Make window visible
     XMapWindow(display, window);
+    XMapWindow(display, window_1);
 
     // Set window title
     char title[FILENAME_MAX];
     sprintf(title, "Image Viewer[%s]", argv[1]);
     XStoreName(display, window, title);
+    XStoreName(display, window_1, title);
 
     // Get WM_DELETE_WINDOW atom
     Atom wm_delete = XInternAtom(display, "WM_DELETE_WINDOW", True);
 
     // Subscribe WM_DELETE_WINDOW message
     XSetWMProtocols(display, window, &wm_delete, 1);
+    XSetWMProtocols(display, window_1, &wm_delete, 1);
 
     memset((void*)&scaledImg, 0, sizeof(scaledImg));
+    memset((void*)&fixedscaledImg, 0, sizeof(fixedscaledImg));
     memset((void*)&originImage, 0, sizeof(originImage));
 
     scaledImg.image = NULL;
+    fixedscaledImg.image = NULL;
     originImage.image = NULL;
+
     // To do : call BMPbufferToRGB
     switch (type)
     {
@@ -186,6 +209,10 @@ int main(int argc, char** argv) {
 
         // Print event type
         printf("got event: %s\n", event_names[event.type]);
+        fixedfitToWindowScale(&fixedscaledImg, &originImage);
+        copyToWindowBuffer(&fixedscaledImg);
+
+        visual(display, window_1, windowImageBuffer_2);
 
         // Keyboard
         if (event.type == KeyPress) {
@@ -259,10 +286,14 @@ int main(int argc, char** argv) {
     
     if(windowImageBuffer.image != NULL)
         free(windowImageBuffer.image);
+    if(windowImageBuffer_2.image != NULL)
+        free(windowImageBuffer_2.image);
     if(originImage.image != NULL)
         free(originImage.image);
     if(scaledImg.image != NULL)
         free(scaledImg.image);
+    if(fixedscaledImg.image !=NULL)
+        free(fixedscaledImg.image);
 
     XCloseDisplay(display);
     return 0;
@@ -278,6 +309,18 @@ void refresh(Display* display, Window window)
     image = XCreateImage(display, visual, DefaultDepth(display,DefaultScreen(display)), ZPixmap, 0, (char *)windowImageBuffer.image, windowImageBuffer.width, windowImageBuffer.height, 32, 0);
 
     XPutImage(display, window, gc, image, 0, 0, 0, 0, windowImageBuffer.width, windowImageBuffer.height);
+}
+
+void visual(Display* display, Window window, Image Buffer)
+{
+    XImage *image;
+    int screen = DefaultScreen(display);
+    GC gc = DefaultGC(display, screen);
+    Visual *visual=DefaultVisual(display, 0);
+
+    image = XCreateImage(display, visual, DefaultDepth(display,DefaultScreen(display)), ZPixmap, 0, (char *)Buffer.image, Buffer.width, Buffer.height, 32, 0);
+
+    XPutImage(display, window, gc, image, 0, 0, 0, 0, Buffer.width, Buffer.height);
 }
 
 unsigned long GetPixelValue(unsigned char red, unsigned char green, unsigned char blue)
@@ -368,6 +411,24 @@ ErrorState fitToWindowScale(pImage dest, pImage src)
             }
             
         }
+        printf("ratio: %f", ratio);
+
+        imageScaler(dest, src, ratio);
+        ret = ErrorNone;
+    }
+
+    return ret;
+
+}
+
+ErrorState fixedfitToWindowScale(pImage dest, pImage src)
+{
+    ErrorState ret = ErrorUnexpected;
+    double ratio = 0.5f;
+
+    if((windowImageBuffer.height * windowImageBuffer.width) < (src->width * src->height))
+    {
+        printf("ratio: %f", ratio);
 
         imageScaler(dest, src, ratio);
         ret = ErrorNone;
@@ -389,6 +450,7 @@ void copyToWindowBuffer(pImage src)
     if((src->width != imgWidth) && (src->height != imgHeight))
     {
         memset((void*)windowImageBuffer.image, 0, sizeof(RGB) * windowImageBuffer.height * windowImageBuffer.width);
+        // memset((void*)windowImageBuffer_2.image, 0, sizeof(RGB) * windowImageBuffer_2.height * windowImageBuffer_2.width);
 
         for(i = 0; i < src->height; i++)
         {
@@ -397,6 +459,11 @@ void copyToWindowBuffer(pImage src)
                 windowImageBuffer.image[((i + heightMargin) * windowImageBuffer.width) + j + widthMargin].r = src->image[(i * src->width) + j].r;
                 windowImageBuffer.image[((i + heightMargin) * windowImageBuffer.width) + j + widthMargin].g = src->image[(i * src->width) + j].g;
                 windowImageBuffer.image[((i + heightMargin) * windowImageBuffer.width) + j + widthMargin].b = src->image[(i * src->width) + j].b;
+
+                // windowImageBuffer_2.image[((i + heightMargin) * windowImageBuffer_2.width) + j + widthMargin].r = src->image[(i * src->width) + j].r;
+                // windowImageBuffer_2.image[((i + heightMargin) * windowImageBuffer_2.width) + j + widthMargin].g = src->image[(i * src->width) + j].g;
+                // windowImageBuffer_2.image[((i + heightMargin) * windowImageBuffer_2.width) + j + widthMargin].b = src->image[(i * src->width) + j].b;
+
             }
         }
 
